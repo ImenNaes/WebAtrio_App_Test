e9ad151cc6a82cdccef524c5e51f18dc46debd0d
Imen Naès <imen.naes@web-atrio.com>
2023-01-26 11:42:46 +0100

    Modifs structure code

---
+++ WebAtrio_App_Test/Repository/Implementations/PersonneEmploiRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WebAtrio_App_Test.EF;
+using WebAtrio_App_Test.Entities;
+using WebAtrio_App_Test.Repository.Interfaces;
+
+namespace WebAtrio_App_Test.Repository.Implementations
+{
+    public class PersonneEmploiRepository : IPersonneEmploiRepository
+    {
+        private readonly MyDbContext _dbContext;
+        public PersonneEmploiRepository(MyDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+        public void AjoutemploiPersonne(PersonneEmploi model)
+        {
+            _dbContext.PersonneEmplois.Add(model);
+            _dbContext.SaveChanges();
+        }
+        public IEnumerable<PersonneEmploi> Getlist()
+        {
+            return _dbContext.PersonneEmplois.ToList();
+        }
+    }
+}
